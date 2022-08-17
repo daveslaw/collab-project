8ecef2364f6dd006989ac30a0f83f769eb423251
Dave <daveslaw@gmail.com>
2022-08-17 21:09:38 +0300

    info and icons and basic styling

--- src/components/projectCard/ProjectCard.js
+++ src/components/projectCard/ProjectCard.js
@@ -1,32 +1,60 @@
-import React from 'react';
-import {ProjectCardContainer, ProjectTitle} from './projectCard.styles';
-import {projectInfo, } from '../../data/mock-data';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-regular-svg-icons'
-//import { solid, regular, } from '@fortawesome/fontawesome-svg-core/import.macro' 
-
+import React from "react";
+import {
+	ProjectCardContainer,
+	ProjectTitle,
+	IconWrapper,
+} from "./projectCard.styles";
+import { projectInfo } from "../../data/mock-data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faClock } from "@fortawesome/free-regular-svg-icons";
+import {
+	faHeart as faHeartSolid,
+	faGlobe,
+	faPeopleGroup,
+    faComputer,
+    faLeaf
+} from "@fortawesome/free-solid-svg-icons";
+//import { solid, regular, } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const ProjectCard = () => {
-  
-    const {title, tagline, projectField, type, spokenLanguage, duration, sizeOfTeam} = projectInfo;
-  
-    return (
-    <ProjectCardContainer>
-        <ProjectTitle>
-            <div>{title}</div>
-            <div><FontAwesomeIcon icon={faHeart} />
-            </div>
-        </ProjectTitle>
-        <div>Tagline: {tagline}</div>
-        <div>{projectField}</div>
-        <div>{duration}</div>
-        <div>{sizeOfTeam}</div>
-        <div>{type}</div>
-        <div>{spokenLanguage}</div>
+	const {
+		title,
+		tagline,
+		projectField,
+		type,
+		spokenLanguage,
+		duration,
+		sizeOfTeam,
+	} = projectInfo;
 
+	return (
+		<ProjectCardContainer>
+			<ProjectTitle>
+				<div>{title}</div>
+				<FontAwesomeIcon icon={faHeart} />
+				{/* <FontAwesomeIcon icon={faHeartSolid} />             */}
+			</ProjectTitle>
+			<div>Tagline: {tagline}</div>
+			<IconWrapper>
+				<div><FontAwesomeIcon icon={faLeaf} />{projectField}</div>
+				<div><FontAwesomeIcon icon={faComputer} />{type}</div>
+				<div>
+					<FontAwesomeIcon icon={faGlobe} />
+					{spokenLanguage}
+				</div>
+			</IconWrapper>
+			<IconWrapper>
+				<div>
+					<FontAwesomeIcon icon={faClock} />
+					{duration}
+				</div>
+				<div>
+					<FontAwesomeIcon icon={faPeopleGroup} />
+					{sizeOfTeam}
+				</div>
+			</IconWrapper>
+		</ProjectCardContainer>
+	);
+};
 
-    </ProjectCardContainer>
-  )
-}
-
-export default ProjectCard
+export default ProjectCard;
--- src/components/projectCard/projectCard.styles.js
+++ src/components/projectCard/projectCard.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ProjectCardContainer = styled.div`
 border: red solid 1px;
-color: green;
+color: #0B5570;
 min-width: 601px;
 min-height:284px;
 font-family: 'Poppins', sans-serif;
@@ -13,4 +13,13 @@ export const ProjectTitle = styled.div`
 display: flex;
 flex-flow: row;
 font-size: 24px;
+justify-content: space-between;
+border: red solid 1px;
+
+`
+
+export const IconWrapper = styled.div`
+display: flex;
+flex-flow: row;
+justify-content: space-evenly;
 `
